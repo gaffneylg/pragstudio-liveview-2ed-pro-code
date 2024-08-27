17388ecfbcacd078b3724636963b2dbc10c33637
Liam Gaffney <gaffneylg@gmail.com>
2024-08-27 14:59:11 +0100

    light switch button clicks - video

---
+++ live_view_studio/lib/live_view_studio_web/live/light_live.ex
@@ -0,0 +1,73 @@
+defmodule LiveViewStudioWeb.LightLive do
+  use LiveViewStudioWeb, :live_view
+
+  #mount
+  def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(brightness: 10)
+    {:ok, socket}
+  end
+
+
+  #render
+  def render(assigns) do
+    ~H"""
+    <h1>Front Porch Light</h1>
+    <div id="light">
+      <div class="meter">
+        <span style={"width: #{@brightness}%"}>
+          <%= @brightness %>%
+        </span>
+      </div>
+      <button phx-click="on">
+        <img src="/images/light-on.svg" alt="Light On">
+      </button>
+      <button phx-click="up">
+        <img src="/images/up.svg" alt="Dimmer up">
+      </button>
+      <button phx-click="down">
+        <img src="/images/down.svg" alt="Dimmer down">
+      </button>
+      <button phx-click="off">
+        <img src="/images/light-off.svg" alt="Light Off">
+      </button>
+
+    </div>
+    """
+  end
+
+  #handle_event
+  def handle_event("on", _, socket) do
+    socket =
+      socket
+      |> assign(brightness: 50)
+    {:noreply, socket}
+  end
+
+  def handle_event("off", _, socket) do
+    socket =
+      socket
+      |> assign(brightness: 0)
+    {:noreply, socket}
+  end
+
+  def handle_event("up", _, socket) do
+    socket =
+      socket
+      |> update(:brightness, &(&1 + 10))
+    {:noreply, socket}
+  end
+
+  def handle_event("down", _, socket) do
+    socket =
+      socket
+      |> update(:brightness, &(&1 - 10))
+    {:noreply, socket}
+  end
+
+  def handle_event(event, _unsigned_params, socket) do
+    IO.inspect("Catch all handle_evenet hit with: #{event}")
+    {:noreply, socket}
+  end
+end
