247f7b64524855105847ca795c7b96e6ac94f83d
Liam Gaffney <gaffneylg@gmail.com>
2024-09-02 08:58:38 +0100

    filtering athletes - exercise

--- live_view_studio/lib/live_view_studio_web/live/athletes_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/athletes_live.ex
@@ -10,14 +10,14 @@ defmodule LiveViewStudioWeb.AthletesLive do
         athletes: Athletes.list_athletes()
       )
 
-    {:ok, socket}
+    {:ok, socket, temporary_assigns: [athletes: []]}
   end
 
   def render(assigns) do
     ~H"""
     <h1>Athletes</h1>
     <div id="athletes">
-      <form>
+      <form phx-change="sport-filter">
         <div class="filters">
           <select name="sport">
             <%= Phoenix.HTML.Form.options_for_select(
@@ -53,6 +53,18 @@ defmodule LiveViewStudioWeb.AthletesLive do
       </div>
     </div>
     """
+  end
+
+  def handle_event("sport-filter", params, socket) do
+    %{"sport" => sport, "status" => status} = params
+    filter = %{sport: sport, status: status}
+    athletes = Athletes.list_athletes(filter)
+    socket =
+      socket
+      |> assign(athletes: athletes)
+      |> assign(filter: filter)
+
+    {:noreply, socket}
   end
 
   defp sport_options do
