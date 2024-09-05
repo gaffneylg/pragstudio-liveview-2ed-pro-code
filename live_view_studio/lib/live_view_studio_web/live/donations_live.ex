b5f7a0e37e47930e447b12cff671609c63eb68c1
Liam Gaffney <gaffneylg@gmail.com>
2024-09-05 18:40:41 +0100

    sorting - video

--- live_view_studio/lib/live_view_studio_web/live/donations_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/donations_live.ex
@@ -4,13 +4,36 @@ defmodule LiveViewStudioWeb.DonationsLive do
   alias LiveViewStudio.Donations
 
   def mount(_params, _session, socket) do
-    donations = Donations.list_donations()
+    {:ok, socket, temporary_assigns: [donations: []]}
+  end
+
+  def handle_params(params, _uri, socket) do
+    sort_by = (params["sort_by"] || "id") |> String.to_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+
+
+    options = %{sort_by: sort_by, sort_order: sort_order}
+
+    donations = Donations.list_donations(options)
 
     socket =
-      assign(socket,
-        donations: donations
-      )
+      socket
+      |> assign(donations: donations)
+      |> assign(options: options)
 
-    {:ok, socket}
+
+    {:noreply, socket}
   end
+
+  def sort_link(assigns) do
+    ~H"""
+      <.link patch={~p"/donations?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"} >
+        <%= render_slot(@inner_block) %>
+      </.link>
+    """
+  end
+
+  defp next_sort_order(:asc), do: :desc
+  defp next_sort_order(:desc), do: :asc
+
 end
