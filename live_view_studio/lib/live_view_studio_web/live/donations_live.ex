1f6f9a9212a5e5f371edebd7802278bb58513b20
Liam Gaffney <gaffneylg@gmail.com>
2024-09-06 17:17:50 +0100

    pagination of donations - video

--- live_view_studio/lib/live_view_studio_web/live/donations_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/donations_live.ex
@@ -11,7 +11,15 @@ defmodule LiveViewStudioWeb.DonationsLive do
     sort_by = valid_sort_by(params)
     sort_order = valid_sort_order(params)
 
-    options = %{sort_by: sort_by, sort_order: sort_order}
+    page = (params["page"] || "1") |> String.to_integer()
+    per_page = (params["per_page"] || "10") |> String.to_integer()
+
+    options = %{
+      sort_by: sort_by,
+      sort_order: sort_order,
+      page: page,
+      per_page: per_page
+    }
 
     donations = Donations.list_donations(options)
 
@@ -20,6 +28,13 @@ defmodule LiveViewStudioWeb.DonationsLive do
       |> assign(donations: donations)
       |> assign(options: options)
 
+    {:noreply, socket}
+  end
+
+  def handle_event("per-page", %{"per-page" => per_page}, socket) do
+    params = socket.assigns.options
+
+    socket = push_patch(socket, to: ~p"/donations?#{%{params | per_page: per_page}}")
 
     {:noreply, socket}
   end
@@ -29,11 +44,20 @@ defmodule LiveViewStudioWeb.DonationsLive do
   slot :inner_block, required: true
 
   def sort_link(assigns) do
+    params = %{
+      assigns.options
+      | sort_by: assigns.sort_by,
+        sort_order: next_sort_order(assigns.options.sort_order)}
+
+    assigns = assign(assigns, params: params)
+
     ~H"""
-      <.link patch={~p"/donations?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"} >
-        <%= render_slot(@inner_block) %>
-        <%= sort_arrow(@sort_by, @options) %>
-      </.link>
+    <.link patch={
+      ~p"/donations?#{@params}"
+    }>
+      <%= render_slot(@inner_block) %>
+      <%= sort_arrow(@sort_by, @options) %>
+    </.link>
     """
   end
 
@@ -45,27 +69,26 @@ defmodule LiveViewStudioWeb.DonationsLive do
   defp next_sort_order(:desc), do: :asc
 
   defp sort_arrow(column, %{sort_by: sort_by, sort_order: sort_order})
-    when column == sort_by do
-      arrow(sort_order)
-    end
+       when column == sort_by do
+    arrow(sort_order)
+  end
+
   defp sort_arrow(_, _), do: ""
 
   defp arrow(:asc), do: "⬆"
   defp arrow(:desc), do: "⬇"
 
-
   defp valid_sort_by(%{"sort_by" => sort_by})
-      when sort_by in ~w(item quantity days_until_expires) do
+       when sort_by in ~w(item quantity days_until_expires) do
     String.to_atom(sort_by)
   end
 
   defp valid_sort_by(_params), do: :id
 
   defp valid_sort_order(%{"sort_order" => sort_order})
-      when sort_order in ~w(asc desc) do
+       when sort_order in ~w(asc desc) do
     String.to_atom(sort_order)
   end
 
   defp valid_sort_order(_params), do: :asc
-
 end
